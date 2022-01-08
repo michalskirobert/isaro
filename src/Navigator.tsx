b7b6ad748cd49ec4b33cbb3d597547f833cb5025
Robert <majointa@gmail.com>
2022-01-08 15:30:34 +0100

    delete rubbish

--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -1,4 +1,3 @@
-import Home from "@components/home";
 import { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -13,33 +12,7 @@ export const Navigator = () => (
             <Route key={path} {...{ element, path, index }} />
           ))}
         </Route>
-      </Routes>{" "}
+      </Routes>
     </Suspense>
   </BrowserRouter>
 );
-{
-  /* <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<App />}>
-      <Route index element={<Home />} />
-      <Route path="teams" element={<Teams />}>
-        <Route path=":teamId" element={<Team />} />
-        <Route path="new" element={<NewTeamForm />} />
-        <Route index element={<LeagueStandings />} />
-      </Route>
-    </Route>
-  </Routes>
-  </BrowserRouter>, */
-}
-
-{
-  /* <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="users" element={<Users />}>
-      <Route path="me" element={<OwnUserProfile />} />
-      <Route path=":id" element={<UserProfile />} />
-    </Route>
-  </Routes>
-</BrowserRouter>; */
-}
