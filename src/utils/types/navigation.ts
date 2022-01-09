9b09e04172b0cb1f51b66c9803c1493c55c327e5
Robert <majointa@gmail.com>
2022-01-09 16:17:20 +0100

    change local paths

--- src/utils/types/navigation.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export type NavigationType = NavigationItem[];
-
-export type NavigationItem = {
-  path: string;
-  component: ReactNode;
-  index: boolean;
-};
